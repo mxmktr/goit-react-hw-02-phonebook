b5c4c922d76bfcfd3cfe337813ed5d2683d29ec7
Maksim Katrich <mxmktr@gmail.com>
2023-04-17 21:19:30 +0300

    add filter

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,15 @@ import Contacts from './Contacts/Contacts';
 
 export default class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
+    filter: '',
   };
 
   addNameToList = event => {
@@ -60,9 +66,25 @@ export default class App extends Component {
             Add contact
           </button>
         </form>
-        {this.state.contacts.length !== 0 && (
-          <Contacts contacts={this.state.contacts} />
-        )}
+        <div>
+          <p>Contacts</p>
+          <label>
+            Find contacts by name
+            <input
+              type="text"
+              name="filter"
+              value={this.state.filter}
+              onChange={this.inputData}
+            />
+          </label>
+
+          {this.state.contacts.length !== 0 && (
+            <Contacts
+              contacts={this.state.contacts}
+              filter={this.state.filter}
+            />
+          )}
+        </div>
       </div>
     );
   }
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-export default function Contacts({ contacts }) {
+export default function Contacts({ contacts, filter }) {
   return (
     <ul>
-      {contacts.map(({ id, name, number }) => (
+      {filterContacts(contacts, filter).map(({ id, name, number }) => (
         <li key={id}>
           {name}: {number}
         </li>
       ))}
     </ul>
+  );
+}
+
+function filterContacts(contacts, filter) {
+  if (!filter) {
+    return contacts;
+  }
+  return contacts.filter(
+    ({ name }) => name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
   );
 }
